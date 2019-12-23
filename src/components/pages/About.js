77c3ec5513a7391c87c8e55ece7d06666862005c
abdeali88 <38809496+abdeali88@users.noreply.github.com>
2019-12-23 12:29:17 +0530

    Add files via upload

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,1 +1,14 @@
+import React from 'react'
 
+
+function About() {
+    return (
+        <>
+        <h1>About</h1>
+        <p>This is a todo list app. You can add and delete todos or mark it as complete. </p>
+            <p>It is importing todos from JSON Plceholder API.</p>
+        </>
+    )
+}
+
+export default About
