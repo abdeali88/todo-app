84cb18772fafa220ee32b3f042feb5dd73a549e2
abdeali88 <38809496+abdeali88@users.noreply.github.com>
2019-12-23 12:32:54 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,37 +1,73 @@
 import React, { Component } from 'react';
 import './App.css';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
 import Todos from './components/Todos';
+import Header from './components/Header';
+import AddComp from './components/AddComp';
+import About from './components/pages/About';
+import axios from 'axios';
 
 class App extends Component {
     constructor(props) {
         super(props)
     
         this.state = {
-              todos:[
-                  {
-                      id:1,
-                      title:'Walk',
-                      completed:true
-                  },
-                  {
-                    id:2,
-                    title:'Cadio',
-                    completed:false
-                },
-                {
-                    id:3,
-                    title:'Run',
-                    completed:false
-                }
-              ]
+              todos:[]
         }
     }
+
+    componentDidMount() {
+        axios.get('https://jsonplaceholder.typicode.com/todos?_limit=10')
+        .then(response => this.setState({todos:response.data}))
+    }
+
+    markComplete = (id) => {
+        this.setState({
+            todos:this.state.todos.map(todo =>  {
+                if(todo.id===id){
+                    todo.completed=!todo.completed
+                }
+                return todo;
+            })
+        })
+        }
+
+    delTodo = (id) => {
+        axios.delete(`https://jsonplaceholder.typicode.com/todos/${id}`)
+        .then(response => this.setState({
+            todos:[...this.state.todos.filter((todo) => todo.id!==id)]
+        })) 
+    }
+
+    addTodo = (title) => {
+        axios.post('https://jsonplaceholder.typicode.com/todos',{
+            title: title,
+            completed: false,
+        })
+        .then(response => this.setState({todos: [...this.state.todos, response.data]}) )
+       
+    }
+    
     
     render() {
         return ( 
+            <Router>
             <div className = "App">
-                <Todos todos={this.state.todos} />
+                <Header/>
+
+                <Route exact path="/" render={props => (
+                    <>
+                     <AddComp addTodo={this.addTodo} />
+                     <Todos todos={this.state.todos} markComplete={this.markComplete} delTodo={this.delTodo}  />
+                     </>
+                )} />
+
+                <Route path="/about" render={props => (
+                    <About />
+                )} />
+               
             </div>
+            </Router>
         )
     }
 }
